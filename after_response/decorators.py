1aa2b241d760c85b4fe2e6330c24ac6c6eeb4034
Aron Jones <aron.jones@gmail.com>
2014-10-23 15:53:46 -0400

    added AFTER_RESPONSE_IMMEDIATE setting

--- after_response/decorators.py
+++ after_response/decorators.py
@@ -1,8 +1,16 @@
 
+from django.conf import settings
 from .store import function_queue
+
+
+AFTER_RESPONSE_IMMEDIATE = getattr(settings, 'AFTER_RESPONSE_IMMEDIATE', False)
+
 
 def enable(func):
     def after_response(*args, **kwargs):
-        function_queue.append((func, args, kwargs))
+        if AFTER_RESPONSE_IMMEDIATE:
+            func(*args, **kwargs)
+        else:
+            function_queue.append((func, args, kwargs))
     func.after_response = after_response
     return func
